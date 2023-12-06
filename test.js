6c7c397e41e8a46677603b931a7a729d2de79652
dbreitel <dbreitel@users.noreply.github.com>
2023-12-06 13:34:18 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const currentDate = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+  const randomNum = Math.floor(Math.random() * 28) + 1; // Generate random number between 1 and 28
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ currentDate, randomNum }),
+  
+  };
+  console.log(response);
+  return response;
